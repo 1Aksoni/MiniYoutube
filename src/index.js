b585d3dca98582d55b0636f849826bc7875540bd
1Aksoni <akshat2001soni@gmail.com>
2024-09-01 23:24:28 +0530

    util code integrigation

---
+++ src/index.js
@@ -0,0 +1,76 @@
+// require('dotenv').config({path: './env'})
+
+// alternate of above 
+import dotenv from "dotenv";
+
+import connectDB from "./db/index.js";
+
+// This is the part of the first import statement
+dotenv.config({
+    path: './env'
+})
+
+
+// It returns a Promise thats why we use then and catch here and also we connect it with the port 
+connectDB()
+.then(()=>{
+    
+    //  This is for communication check on PORT 
+     app.on("error",()=>{
+          console.log("Error: ", error);
+          throw error;
+     })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGODB connection failed!!",err)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+
+const app = express();
+
+;(async ()=>{
+ try{
+    // This is to connect the Data Base
+     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+    //  This is for communication check on PORT 
+     app.on("error",()=>{
+          console.log("Error: ", error);
+          throw error;
+     })
+
+     app.listen(process.env.PORT,()=>{
+        console.log(`App is listening on port ${process.env.PORT}`);
+     })
+ }
+ catch (error){
+       console.log("ERROR: ", error);
+       throw error;
+ }
+})()
+ */
